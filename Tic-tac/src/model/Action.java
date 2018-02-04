7ded64e5c14c00a013f2fc24ad8bf8741227fc4d
Andrew-booler <andrew.zhangw@gmail.com>
2018-02-03 20:16:17 -0500

    first post

---
+++ Tic-tac/src/model/Action.java
@@ -0,0 +1,13 @@
+package model;
+
+
+
+public class Action {
+	private State previous;
+	private State post;
+	private int mark;
+	private int postion;
+	public State move(State pre, int mark, int position) {
+		return post;
+	}
+}
---
+++ Tic-tac/src/model/State.java
@@ -0,0 +1,43 @@
+package model;
+
+public class State {
+	private int[] board;
+	private int turn;
+	private int utility;
+	/**
+	 * @return board
+	 */
+	public int[] getBoard() {
+		return board;
+	}
+	/**
+	 * @param board 要设置的 board
+	 */
+	public void setBoard(int[] board) {
+		this.board = board;
+	}
+	/**
+	 * @return utility
+	 */
+	public int getUtility() {
+		return utility;
+	}
+	/**
+	 * @param utility 要设置的 utility
+	 */
+	public void setUtility(int utility) {
+		this.utility = utility;
+	}
+	/**
+	 * @return turn
+	 */
+	public int getTurn() {
+		return turn;
+	}
+	/**
+	 * @param turn 要设置的 turn
+	 */
+	public void setTurn(int turn) {
+		this.turn = turn;
+	}
+}
